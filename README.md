d8fbcd9c84a49b4782cac38e46eb17774ef87984
dhidas <dhidas@dhidas.com>
2018-08-13 15:03:29 -0400

    adding file download more readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # IDCommon_ivu23_2.8m
+
+
+### Nomenclature / Abbreviations
+
+USU : Up-Stream Upper
+DSU : Down-Stream Upper
+USL : Up-Stream Lower
+DSL : Down-Stream Lower
+
+
---
+++ pmc/FileDownload_id16_LIX_A13004.h
@@ -0,0 +1,17 @@
+; ===========================================================
+;	Download all files to pmac
+; ===========================================================
+
+
+; ------------
+; (#3)[A13004]
+; ------------
+
+; Linear encoder offsets particular to this device
+#define LINENC_OFFSET_USU 70582604
+#define LINENC_OFFSET USL 73154733
+#define LINENC_OFFSET_DSU 71251126
+#define LINENC_OFFSET_DSL 71941200
+
+
+
