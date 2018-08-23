4d1d6fb9374d49f1dc909011f5195e9e4a90cb62
Dean Andrew Hidas <dhidas@id03.bnl.gov>
2018-08-23 08:26:32 -0400

    adding uncommit for testing

--- iocBoot/iocIVU23/st.cmd
+++ iocBoot/iocIVU23/st.cmd
@@ -19,7 +19,25 @@ IVU23_registerRecordDeviceDriver(pdbbase)
 
 # Configure ports
 pmacAsynIPConfigure("P0", $(PMAC1_IP))
-pmacCreateController("PMAC1", "P0", 0, 9, 50, 500)
+pmacCreateController("PMAC1", "P0", 0, 5, 50, 500)
+
+pmacCreateAxis("PMAC1", 1)
+pmacCreateAxis("PMAC1", 2)
+pmacCreateAxis("PMAC1", 3)
+pmacCreateAxis("PMAC1", 4)
+pmacCreateAxis("PMAC1", 5)
+
+dbLoadRecords("../../db/motor.db","P=$(sys),M={$(dev)-Ax:5},MOTOR=PMAC1,ADDR=5,DESC=Virtual Motor,DTYP=asynMotor,MRES=0.001,ERES=0.001,EGU=um")
+dbLoadRecords("../../db/motorstatus.db","SYS=$(sys),DEV={$(dev)-Ax:5},PORT=P0,AXIS=5")
+dbLoadRecords("../../db/pmacStatus.db","SYS=$(sys),PMAC=$(dev),VERSION=1,PLC=5,NAXES=8,PORT=P0")
+dbLoadRecords("../../db/pmac_asyn_motor.db","SYS=$(sys),DEV={$(dev)-Ax:5},ADDR=5,SPORT=P0,DESC=motor5,PREC=5,EGU=cts")
+
+
+
+
+
+
+
 
 ## Load record instances
 dbLoadRecords("../../db/IVU23.db","SYS=ID,DEV=IVU23:1,PORT=P0")
@@ -34,8 +52,8 @@ set_pass1_restoreFile("ioc_waveforms.sav")
 
 
 # Set Idle and Moving poll periods (CS_Ref, PeriodMilliSeconds)
-#pmacSetCoordIdlePollPeriod(0, 500)
-#pmacSetCoordMovingPollPeriod(0, 100)
+pmacSetCoordIdlePollPeriod(0, 500)
+pmacSetCoordMovingPollPeriod(0, 100)
 
 dbLoadRecords("../../db/asynRecord.db","P=$(sys),R={$(dev)}Asyn,ADDR=1,PORT=P0,IMAX=128,OMAX=128")
 
